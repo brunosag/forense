43aa209a5b52c7e31d71bace2a3c1e75ee16dd87
brunosag <brunosag02@gmail.com>
2023-11-08 21:07:02 -0300

    feat: add logo to favicon and login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,14 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/app/components/ui/card';
+import Image from 'next/image';
 import LoginForm from './login-form';
+import logo from '@/public/logo.svg';
 
 export default function Login() {
   return (
     <div className="container min-h-screen flex justify-center items-center">
       <Card className="w-full max-w-md border-none">
-        <CardHeader className="text-center">
+        <CardHeader className="items-center">
+          <Image src={logo} alt="SBCB Lab Logo" className="w-16 h-16 mb-3" />
           <CardTitle>Sign in</CardTitle>
           <CardDescription>Please enter your credentials</CardDescription>
         </CardHeader>
